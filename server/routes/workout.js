97b9951066cce25061324732e8b99e077afb07a2
paresiqbal <thecrowdel@gmail.com>
2023-03-31 13:53:45 +0700

    Adding authorization for content

--- server/routes/workout.js
+++ server/routes/workout.js
@@ -6,8 +6,10 @@ import {
   deleteWorkout,
   updateWorkout,
 } from "../controllers/workoutController.js";
-
+import { requireAuth } from "../middleware/requireAuth.js";
 const router = express.Router();
+
+router.use(requireAuth);
 
 // Get all workout
 router.get("/", getWorkout);
